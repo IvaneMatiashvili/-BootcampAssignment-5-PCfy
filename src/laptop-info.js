13448f8c2d156b086d4f261252e324d3437f8c72
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-08-29 22:17:08 +0400

    feat: create a image upload and load generator function
    this function displays upload image and saves image to local storage

--- src/laptop-info.js
+++ src/laptop-info.js
@@ -4,16 +4,78 @@ const laptopInfo = document.querySelector('.laptop-info');
 const employeeLine = document.querySelector('.employee-line');
 const laptopLine = document.querySelector('.laptop-line');
 
+const imageInput = document.querySelector('.upload-img');
+const imgFormContainer = document.querySelector('.img-form-container');
+const imgForm = document.querySelector('.img-form');
+
 const goBackBtn = document.querySelector('.go-back-btn');
 const submitContainer = document.querySelector('.submit-container');
 
 
 const { log: l } = console;
 
-const changePageHelper = () => {
-        submitContainer?.addEventListener('click', () => {
-            window.location.href = './successfully-add.html';
+
+
+
+
+const imgUploadGenerator = () => {
+
+    let uploadedImage = "";
+    imageInput.addEventListener('change', function () {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+            uploadedImage = reader.result;
+            localStorage.setItem('resent-image', uploadedImage);
+
+            imgFormContainer.style.backgroundImage = `url(${uploadedImage})`;
+            imgForm.style.opacity = '0';
         })
+        reader.readAsDataURL(this.files[0]);
+    })
+
+    if (localStorage.getItem('resent-image')) {
+        imgFormContainer.style.backgroundImage = `url(${localStorage.getItem('resent-image')})`;
+        imgForm.style.opacity = '0';
+    }
+
+}
+
+imgUploadGenerator();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const changePageHelper = () => {
+    submitContainer?.addEventListener('click', () => {
+        window.location.href = './successfully-add.html';
+    })
 }
 
 changePageHelper();
@@ -23,7 +85,7 @@ const showEmployeeInfo = () => {
     goBackBtn?.addEventListener('click', () => {
         laptopInfo.style.display = 'none';
         employeeInfo.style.display = 'flex';
-        
+
         localStorage.setItem('info-page-content', '0')
 
         laptopLine.style.display = 'none';
