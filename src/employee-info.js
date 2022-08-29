0d90d6cbbc696cff097feefbd10e9897b520ffea
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-08-29 20:13:24 +0400

    feat: change nav line display

--- src/employee-info.js
+++ src/employee-info.js
@@ -1,5 +1,9 @@
-const employeeInfo = document.querySelector('.employee-info')
-const laptopInfo = document.querySelector('.laptop-info')
+const employeeInfo = document.querySelector('.employee-info');
+const laptopInfo = document.querySelector('.laptop-info');
+
+const employeeLine = document.querySelector('.employee-line');
+const laptopLine = document.querySelector('.laptop-line');
+
 const nextBtnBackgroundContainer = document.querySelector('.next-btn-background-container');
 const nextBtn = document.querySelector('.next-btn');
 
@@ -10,11 +14,17 @@ const showLaptopInfo = () => {
     nextBtn?.addEventListener('click', () => {
         employeeInfo.style.display = 'none';
         laptopInfo.style.display = 'flex';
+        
+        employeeLine.style.display = 'none';
+        laptopLine.style.display = 'flex';
     })
     
     nextBtnBackgroundContainer?.addEventListener('click', () => {
         employeeInfo.style.display = 'none';
         laptopInfo.style.display = 'flex';
+        
+        employeeLine.style.display = 'none';
+        laptopLine.style.display = 'flex';
     })
 }
 
